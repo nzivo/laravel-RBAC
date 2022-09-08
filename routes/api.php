3b8cea63897ea54cc97d2ab3b7674550d48b8b39
John Nzivo <johnnnzivo@gmail.com>
2022-09-08 13:31:12 +0300

    Added JWT

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$api = app('Dingo\Api\Routing\Router');
+$api -> version('v1', function($api){
+    $api -> get('hello', function(){
+        return 'test';
+    });
+});
