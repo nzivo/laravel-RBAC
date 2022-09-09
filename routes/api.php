8487bded0d850bbd112ca01a401c94c5782c761b
John Nzivo <johnnnzivo@gmail.com>
2022-09-09 09:14:17 +0300

    Added UserProfile management

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 $api = app('Dingo\Api\Routing\Router');
 $api -> version('v1', function($api){
-    $api-> post('/users/signup', 'App\Http\Controllers\UserController@store');
-    $api-> post('/users/login', 'App\Http\Controllers\Auth\AuthController@login');
 
-    $api->group(['middleware' => 'api', 'prefix'  => 'auth'], function($api){
-        $api-> post('token/refresh', 'App\Http\Controllers\Auth\AuthController@refresh');
-        $api-> post('logout', 'App\Http\Controllers\Auth\AuthController@logout');
+    $api->group(['prefix' => 'auth'], function($api) {
+        $api-> post('/signup', 'App\Http\Controllers\UserController@store');
+        $api-> post('/login', 'App\Http\Controllers\Auth\AuthController@login');
+        $api->group(['middleware' => 'api.auth'],
+            function($api){
+                $api-> post('/refresh-token', 'App\Http\Controllers\Auth\AuthController@refresh');
+                $api-> post('/logout', 'App\Http\Controllers\Auth\AuthController@logout');
+        });
     });
 
-    $api->group(['middleware' => ['role:super-admin'], 'prefix' => 'admin'], function($api){
-        $api->get('users', 'App\Http\Controllers\Admin\AdminUserController@index');
-    });
+    $api->group(['middleware' => ['role:super-admin'], 'prefix' => 'admin'],
+        function($api){
+            $api->get('/users', 'App\Http\Controllers\Admin\AdminUserController@index');
+        }
+    );
 });
